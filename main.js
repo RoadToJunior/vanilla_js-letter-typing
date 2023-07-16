a3d0bf09a3f0fa7e0e63249e0b18e5daf26b876d
NowWeAre-Free <kontodarm@wp.pl>
2023-07-16 21:19:39 +0200

    add button and event listener to it

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const spnText = document.querySelector(".text");
+const btn = document.querySelector("button");
 const txt =
   "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? KONIEC";
 
 let number = 0;
+let indexTyping;
+
 const addLetter = () => {
   spnText.textContent += txt[number];
   number++;
@@ -11,4 +14,6 @@ const addLetter = () => {
   }
 };
 
-const indexTyping = setInterval(addLetter, 50);
+btn.addEventListener("click", () => {
+  indexTyping = setInterval(addLetter, 50);
+});
