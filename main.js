7ff7cc7298ef71d5b8a35135380e22998ee845fc
NowWeAre-Free <kontodarm@wp.pl>
2023-07-16 20:42:37 +0200

    add setInterval

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const spnText = document.querySelector(".text");
 const txt =
   "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? KONIEC";
+
+let number = 0;
+const addLetter = () => {
+  spnText.textContent += txt[number];
+  number++;
+  if (number === txt.length) {
+    clearInterval(indexTyping);
+  }
+};
+
+const indexTyping = setInterval(addLetter, 50);
