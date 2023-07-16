c46b3de0cb80d56c1b01c922bafafc627d76ee11
NowWeAre-Free <kontodarm@wp.pl>
2023-07-16 22:28:14 +0200

    add typing cursor animation

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const spnText = document.querySelector(".text");
+const spnCursor = document.querySelector(".cursor");
 const btn = document.querySelector("button");
 const txt =
   "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? KONIEC";
 
 let number = 0;
-let indexTyping;
 const time = 40;
 
 const addLetter = () => {
@@ -15,6 +15,9 @@ const addLetter = () => {
   }
 };
 
-btn.addEventListener("click", () => {
-  indexTyping = setInterval(addLetter, time);
-});
+const cursorAnimation = () => {
+  spnCursor.classList.toggle("active");
+};
+
+const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
